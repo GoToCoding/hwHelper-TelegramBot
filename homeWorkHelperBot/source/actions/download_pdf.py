dce7ad435c3b497acd61a562b80596703f29ee97
GoToCoding <ruzal.ibragimov@gmail.com>
2018-09-30 00:59:03 +0300

    one more time

--- homeWorkHelperBot/source/actions/download_pdf.py
+++
@@ -1,7 +0,0 @@
-import urllib.request
-
-
-def download_file(download_url, filename):
-    urllib.request.urlretrieve(download_url, '/home/ruzal/Workspace/projects/pythonProj1/homeWorkHelperBot'
-                                             '/homeWorkHelperBot/files/' + filename)
-    return 'OK'
--- homeWorkHelperBot/source/classes/user.py
+++
@@ -1,7 +0,0 @@
-class User:
-    def __init__(self, name, uid):
-        self.name = name
-        self.uid = uid
-        self.groups = []
-        self.last_markup = None
-        self.selected_group = None
