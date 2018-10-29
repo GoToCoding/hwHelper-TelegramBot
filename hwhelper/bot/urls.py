8a71c18bfa29a0f05c61da81fb5bb9522ace51d6
Nikita <the@fusy.ru>
2018-10-29 17:47:11 +0300

    init django

---
+++ hwhelper/bot/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+
+from bot import views
+
+
+urlpatterns = [
+    path('webhook/', views.webhook)
+]
---
+++ hwhelper/bot/views.py
@@ -0,0 +1,5 @@
+from django.http import HttpResponseNotFound
+
+
+def webhook(request):
+    return HttpResponseNotFound('Not implemented yet.')
