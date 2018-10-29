eebc3650cc223c9fa4868e6aa767bbe6e693caec
Nikita <the@fusy.ru>
2018-10-29 20:21:39 +0300

    add some models

--- hwhelper/bot/models.py
+++ hwhelper/bot/models.py
@@ -1,3 +1,30 @@
 from django.db import models
 
-# Create your models here.
+
+class User(models.Model):
+    username = models.CharField(max_length=128, null=True, unique=True)
+    uid = models.CharField(max_length=128, unique=True)
+    state = models.CharField(max_length=128)
+
+
+class Group(models.Model):
+    pass
+
+
+class Deadline(models.Model):
+    name = models.CharField(max_length=128)
+    group = models.ForeignKey(to=Group)
+    date_from = models.DateField()
+    date_to = models.DateField()
+
+
+class GroupEntry(models.Model):
+    user = models.ForeignKey(to=User)
+    group = models.ForeignKey(to=Group)
+    name = models.CharField(max_length=128)
+    order = models.IntegerField(default=0)
+
+
+class File(models.Model):
+    deadline = models.ForeignKey(to=Deadline)
+    file_id = models.CharField(max_length=128, unique=True)
