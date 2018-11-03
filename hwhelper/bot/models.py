256cd2460cfad5b52b16420b888c97bcaf11d075
Nikita <the@fusy.ru>
2018-11-04 00:08:29 +0300

    add migrations

--- hwhelper/bot/models.py
+++ hwhelper/bot/models.py
@@ -13,18 +13,18 @@ class Group(models.Model):
 
 class Deadline(models.Model):
     name = models.CharField(max_length=128)
-    group = models.ForeignKey(to=Group)
+    group = models.ForeignKey(to=Group, on_delete=models.CASCADE)
     date_from = models.DateField()
     date_to = models.DateField()
 
 
 class GroupEntry(models.Model):
-    user = models.ForeignKey(to=User)
-    group = models.ForeignKey(to=Group)
+    user = models.ForeignKey(to=User, on_delete=models.CASCADE)
+    group = models.ForeignKey(to=Group, on_delete=models.CASCADE)
     name = models.CharField(max_length=128)
     order = models.IntegerField(default=0)
 
 
 class File(models.Model):
-    deadline = models.ForeignKey(to=Deadline)
+    deadline = models.ForeignKey(to=Deadline, on_delete=models.CASCADE)
     file_id = models.CharField(max_length=128, unique=True)
